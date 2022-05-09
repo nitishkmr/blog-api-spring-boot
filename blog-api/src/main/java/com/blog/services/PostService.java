2ea9b30b34eb936595c6c83bbf0bc80b1e8f858a
Nitish <nitishkmr096@gmail.com>
2022-05-09 18:18:43 +0530

    Add Sorting in getAllPosts()

--- blog-api/src/main/java/com/blog/services/PostService.java
+++ blog-api/src/main/java/com/blog/services/PostService.java
@@ -10,7 +10,7 @@ import java.util.List;
 public interface PostService {
 
     PostDto getPostById(Integer postId);
-    PostResponse getAllPosts(Integer pageNumber, Integer pageSize);
+    PostResponse getAllPosts(Integer pageNumber, Integer pageSize, String sortBy, String sortDir);
     List<PostDto> getPostsByCategory(Integer categoryId);
     List<PostDto> getPostsByUser(Integer userId);
     PostDto createPost(PostDto postDto, Integer userId, Integer categoryId);
