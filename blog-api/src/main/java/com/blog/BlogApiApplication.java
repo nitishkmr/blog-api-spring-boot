2b6626b1c2085f95699d4f278eb57c90495a90ff
Nitish <nitishkmr096@gmail.com>
2022-05-12 02:04:23 +0530

    Add Database and roles to Basic Auth

--- blog-api/src/main/java/com/blog/BlogApiApplication.java
+++ blog-api/src/main/java/com/blog/BlogApiApplication.java
@@ -1,12 +1,19 @@
 package com.blog;
 
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
-public class BlogApiApplication {
+public class BlogApiApplication implements CommandLineRunner {
+
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+
 	public static void main(String[] args) {
 		SpringApplication.run(BlogApiApplication.class, args);
 	}
@@ -15,4 +22,8 @@ public class BlogApiApplication {
 		return new ModelMapper();
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Generated pass: " + this.passwordEncoder.encode("12345"));
+	}
 }
